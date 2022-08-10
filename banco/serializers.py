dbd60c12aceb13bffa554f3937e5f1e9218d6f31
Gusta03 <90052920+Gusta03@users.noreply.github.com>
2022-08-10 17:14:14 -0300

    fix/validacoes_bd_transacoes

--- banco/serializers.py
+++ banco/serializers.py
@@ -1,6 +1,6 @@
-from wsgiref.validate import validator
+from pyexpat import model
 from rest_framework import serializers
-from banco.models import Usuario, ContaBancaria
+from banco.models import Usuario, ContaBancaria, Transacoes
 from banco.validators import *
 
 class UsuarioSerializer(serializers.ModelSerializer):
@@ -14,7 +14,6 @@ class UsuarioSerializer(serializers.ModelSerializer):
         if not nome_valido(data['nome_completo']):
             raise serializers.ValidationError({'nome_completo':"Não inclua números neste campo"})
         return data
-    
 class ContaBancariaSerializer(serializers.ModelSerializer):
     class Meta:
         model = ContaBancaria
@@ -22,4 +21,29 @@ class ContaBancariaSerializer(serializers.ModelSerializer):
         def validate(self, data):
             if not numero_conta(data['numero_conta']):
                 raise serializers.ValidationError({'numero_conta':"Número de Conta inválido, por favor preencha somente com numeros"})
+            else:
+                raise 
             return data
+class ConsultaSaldoSerializer(serializers.ModelSerializer):
+    saldo_total = serializers.ReadOnlyField(source = 'saldo')
+    class Meta:
+        model = ContaBancaria
+        fields = ['saldo_total']
+class ConsultaUserSerializer(serializers.ModelSerializer):
+    Usuario = serializers.ReadOnlyField(source = 'Lojista')
+    class Meta:
+        model = Usuario
+        fields = ['Usuario']
+
+class TransacaoBancariaSerializer(serializers.ModelSerializer):
+    model = Transacoes
+    fields = '__all__'
+    def validate(self, data):
+        if not permissao_user(data['idUsuario_origem']):
+            raise serializers.ValidationError({'idUsuario_origem':"Esse usuario não pode realizar transações."})
+
+        # if not Valida_valorTransferencia(data[''])
+        # return data
+
+
+
--- banco/validators.py
+++ banco/validators.py
@@ -1,8 +1,7 @@
-from operator import truediv
 import re
-from urllib import response
+from urllib.robotparser import RequestRate
+import requests
 from validate_docbr import CPF, CNPJ
-
 
 def cpf_cnpj_valido(numero_do_cpf_cnpj):
 
@@ -17,17 +16,41 @@ def cpf_cnpj_valido(numero_do_cpf_cnpj):
 
     return resultado
 
-# def cnpj_valido(numero_do_cnpj):
-#     cnpj = CNPJ()
-#     return cnpj.validate(numero_do_cnpj)
-
 def nome_valido(nome):
     return nome.isalpha()
 
 def numero_conta(conta):
-
     if conta == int:
         resposta = True
     else:
         resposta = False
     return resposta
+
+def permissao_user(id):
+    tipoUser = requests.get(f'http://127.0.0.1:8000/Usuario/{id}/Tipo/')
+    if tipoUser['Usuario'] == True:
+        resposta = False
+    else:
+        resposta = True
+    return resposta
+
+def Valida_valorTransferencia(idUser, valorTransferencia):
+    saldo = requests.get(f"http://127.0.0.1:8000/SaldoBancario/{idUser}/Saldo/")
+
+    if saldo['saldo_total'] >= valorTransferencia:
+        resposta = True
+    else:
+        resposta = False
+    return resposta
+
+def valida_api():
+    autenticacao = requests.get('https://run.mocky.io/v3/d02168c6-d88d-4ff2-aac6-9e9eb3425e31')
+    if autenticacao['authorization'] == True:
+        resposta = True
+    else:
+        resposta = False
+    return resposta
+
+
+
+ 
